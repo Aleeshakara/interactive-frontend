fa83964a167de88278a683a396293bef54dca1f1
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-10-11 18:44:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const topLeft = document.querySelector('.top-left-button');
+const topRight = document.querySelector('.top-right-button');
+const bottomLeft = document.querySelector('.bottom-left-button');
+const bottomRight = document.querySelector('.bottom-right-button');
 
+const sequence =  [
+  bottomRight,
+  topLeft,
+  bottomLeft,
+  topRight
+];
+
+const flash = button => {
+  return new Promise((resolve, reject) => {
+    button.className += 'active';
+    setTiomeout(() => {
+      button.className = button.className.replace('active', '');
+      resolve();
+    }, 1000);
+  });
+};
+
+const main = async () => {};
+  for (const button of sequence) {  
+    await flash(button);
+  }
+};
+
+main();
+  
