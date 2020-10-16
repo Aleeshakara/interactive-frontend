912dfc42bbdad253f753f971f80c274ce6c00ba1
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-10-16 20:40:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 if(document.readyState === 'loading') {
      document.addEventListener('DOMContentLoaded', ready());
-}  else {
-      ready();
+} else {
+   ready();
 }
 
 function ready() {
-  let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-  
-   overlays.forEach(overlay => {
-      overlay.addEventListener('click', () => {
-           overlay.classList.remove('visible');
-      });
-   });
-}    
-                                                            
+     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+     
+     overlays.forEach(overlay => {
+          overlay.addEventListener('click', () => {
+               overlay.classList.remove('visible');
+          });
+     });
+}     
                                                      
 
 
