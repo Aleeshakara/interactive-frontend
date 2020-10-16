829dfab2103944aa98d15e021c17a53fca09791b
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-10-16 13:35:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+if(document.readyState === 'loading') {
+     document.addEventListener('DOMContentLoaded', ready());
+}  else {
+      ready();
+}
+
+function ready() {
+  Let overlays = Array.from(document.getElementsByClassName('overlay-text));
+  
+   overlays.forEach(overlay => {
+      overlay.addEventListener('click', () => {
+           overlay.classList.remove('visible');
+      });
+   });
+}
+                                                            
+                                                     
+
+
 const topLeft = document.querySelector('.top-left-panel');
 const topRight = document.querySelector('.top-right-panel');
 const bottomLeft = document.querySelector('.bottom-left-panel');
