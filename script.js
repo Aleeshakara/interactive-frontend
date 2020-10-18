c25e9d03f1a9bf7be4cf06f9928eb2fc4bf7475c
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-10-18 23:31:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ function ready() {
      });
 } 
 
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
 
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector(".topleft-panel");
+const topRight = document.querySelector(".topright-panel");
+const bottomLeft = document.querySelector(".bottomleft-panel");
+const bottomRight = document.querySelector(".bottomright-panel");
+const hardButton = document.querySelector("#hard");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
