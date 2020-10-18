712e7e22a60e0809b94e9168f7425472ba70a289
Aleeshakara <58400743+Aleeshakara@users.noreply.github.com>
2020-10-18 23:06:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const startButton = document.querySelector("#start");
 
 hardButton.addEventListener('click', () => {
   if (hardButton.checked == true) {
-    strict = true;
+    hard = true;
   } else {
     hard = false;
   }
